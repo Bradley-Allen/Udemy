c01d90f2de3fb958237209fca86a7839afc177a7
Bradley Allen <btallen08@gmail.com>
2023-08-05 22:15:41 -0400

    added add expense and filter expense

--- React/src/App.js
+++ React/src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -23,9 +24,14 @@ function App() {
     },
   ];
 
+  function addExpenseHandler(expense) {
+    console.log("In App.js");
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
