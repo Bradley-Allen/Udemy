42153cb1fe468a31224fc5ee133ac102d9a8765f
Bradley Allen <btallen08@gmail.com>
2023-09-25 23:14:53 -0400

    added category (add)

---
+++ .NET MVC/ECommerceApp/ECommerceApp/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using ECommerceApp.Data;
+using ECommerceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db) {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The Category Name cannot match the Display Order");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+    }
+}
--- .NET MVC/ECommerceApp/ECommerceApp/obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
+++ .NET MVC/ECommerceApp/ECommerceApp/obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6f705830340d8dd7531787e5a3e002968c14f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
